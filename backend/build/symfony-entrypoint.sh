88a24452f413454e010285e92e551ce2f50a41b7
Algirdas Č <algirdas@cici.lt>
2023-07-06 17:37:05 +0300

    workflow fixes v15

--- backend/build/symfony-entrypoint.sh
+++ backend/build/symfony-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sed "s/{REGENERATE_SECRET}/"$(openssl rand -hex 32)"/" .env.dist
+php bin/console lexik:jwt:generate-keypair -n --skip-if-exists
+
+composer dump-env prod
+
 wait-for-it database:3306 -t 30
 
 php bin/console doctrine:database:create --if-not-exists -n
 php bin/console doctrine:migrations:migrate --allow-no-migration -n
-
-echo 'DONE!'
