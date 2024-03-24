23daf0018218fbdc1ae3a3f44c9afe58b83b3f0e
Algirdas Č <algirdas.cic@gmail.com>
2024-03-25 01:19:12 +0200

    ui update

--- frontend/src/app/util/entity.util.ts
+++ frontend/src/app/util/entity.util.ts
@@ -1,5 +1,6 @@
 import {EntityInterface} from '../api/entities/entity.interface';
 
+// TODO: deprecated
 export class EntityUtil {
     public static replaceInArray(entityArray: EntityInterface[], entity: EntityInterface): void {
         const index = entityArray.findIndex((e: EntityInterface) => e.id === entity.id);
