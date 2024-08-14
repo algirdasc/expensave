35a03c95c44d1e5d9c4012af186294934927558f
Algirdas Č <algirdas.cic@gmail.com>
2024-08-14 20:35:34 +0300

    migrated eslint config

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,73 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["projects/**/*"],
+}, ...compat.extends(
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@angular-eslint/recommended",
+    "plugin:@angular-eslint/template/process-inline-templates",
+    "plugin:prettier/recommended",
+).map(config => ({
+    ...config,
+    files: ["**/*.ts"],
+})), {
+    files: ["**/*.ts"],
+
+    languageOptions: {
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: ["tsconfig.json"],
+            createDefaultProgram: true,
+        },
+    },
+
+    rules: {
+        "@angular-eslint/directive-selector": ["error", {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+        }],
+
+        "@angular-eslint/component-selector": ["error", {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+        }],
+
+        "@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/explicit-member-accessibility": "error",
+        "sort-vars": "error",
+    },
+}, ...compat.extends("plugin:@angular-eslint/template/recommended").map(config => ({
+    ...config,
+    files: ["**/*.html"],
+})), {
+    files: ["**/*.html"],
+    rules: {},
+}, ...compat.extends("plugin:prettier/recommended").map(config => ({
+    ...config,
+    files: ["**/*.html"],
+    ignores: ["**/*inline-template-*.component.html"],
+})), {
+    files: ["**/*.html"],
+    ignores: ["**/*inline-template-*.component.html"],
+
+    rules: {
+        "prettier/prettier": ["error", {
+            parser: "angular",
+        }],
+    },
+}];
