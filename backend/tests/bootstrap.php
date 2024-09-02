adbf885b4f0455246dee03e6abfb6e1fcadb5775
Algirdas Č <algirdas.cic@gmail.com>
2024-09-02 20:53:53 +0300

    feat: code coverage ignore

--- backend/tests/bootstrap.php
+++ backend/tests/bootstrap.php
@@ -6,7 +6,7 @@ use Symfony\Component\ErrorHandler\ErrorHandler;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
+    require_once dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
