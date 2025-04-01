4bb16e154daea07a3b3d30475677035f980205c7
Algirdas Č <algirdas.cic@gmail.com>
2025-04-01 23:08:06 +0300

    fix: monthly report bugfix

--- frontend/src/app/directives/outside-click.directive.ts
+++ frontend/src/app/directives/outside-click.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 
-@Directive({ selector: '[appOutsideClick]' })
+@Directive({ selector: '[appOutsideClick]', standalone: true })
 export class OutsideClickDirective {
     @Output()
     public outsideClick: EventEmitter<MouseEvent> = new EventEmitter();
