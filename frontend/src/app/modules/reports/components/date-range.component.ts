d9510bdd1f60fabe09b2d361dd5c5e2e5159fe5d
Algirdas <algirdas.cic@gmail.com>
2026-02-03 18:37:58 +0200

    fix: bump angular version

--- frontend/src/app/modules/reports/components/date-range.component.ts
+++ frontend/src/app/modules/reports/components/date-range.component.ts
@@ -1,18 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { NbCalendarRange, NbIconModule } from '@nebular/theme';
-import { NgIf, DatePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-date-range',
     template: ` <div class="d-flex justify-content-center">
-        <span *ngIf="dateRange?.start">
-            {{ dateRange.start | date }}
-        </span>
-        <span *ngIf="!dateRange?.start"> Beginning of universe </span>
-        <nb-icon icon="arrow-forward-outline" class="mx-3"></nb-icon>
-        {{ dateRange?.end | date }}
+        @if (dateRange?.start) {
+            <span>{{ dateRange.start | date }}</span>
+        } @else {
+            <span> Beginning of universe </span>
+            <nb-icon icon="arrow-forward-outline" class="mx-3"></nb-icon>
+            {{ dateRange?.end | date }}
+        }
     </div>`,
-    imports: [NgIf, NbIconModule, DatePipe],
+    imports: [NbIconModule, DatePipe],
 })
 export class DateRangeComponent {
     @Input({ required: true })
